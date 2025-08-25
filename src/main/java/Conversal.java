f91777fc3569005e6faac28a8bdda21739bf870c
Anthony Chow <wjchowanthony@gmail.com>
2025-08-25 22:31:45 +0800

    Complete Level-1: Echo

--- src/main/java/Conversal.java
+++ src/main/java/Conversal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Conversal {
 
     public static void main(String[] args) {
@@ -5,10 +7,28 @@ public class Conversal {
         String name = "Conversal";
         String greeting = "What can I do for you?";
         String exit = "Bye! Hope to see you again!";
+        String instruction = "Enter bye to close chatbot.";
+
 
         System.out.println("Hello! I'm " + name + ".");
         System.out.println(greeting + "\n");
-        System.out.println(exit);
+        System.out.println("Instructions: " + instruction + "\n");
 
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.print("User: ");
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;  // Exit when user types Bye
+            }
+
+            System.out.println("Conversal: " + input + "\n");  // Repeat user input
+        }
+
+        System.out.println("\n" + exit);
+        scanner.close();
     }
 }
