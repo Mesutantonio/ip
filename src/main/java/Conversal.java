1e1edeb02445e91d90c0f7a699db939e5822629b
Anthony Chow <wjchowanthony@gmail.com>
2025-08-25 22:17:32 +0800

    Complete level-0: Greeting and Exit

--- src/main/java/Conversal.java
+++ src/main/java/Conversal.java
@@ -3,7 +3,12 @@ public class Conversal {
     public static void main(String[] args) {
 
         String name = "Conversal";
+        String greeting = "What can I do for you?";
+        String exit = "Bye! Hope to see you again!";
 
         System.out.println("Hello! I'm " + name + ".");
+        System.out.println(greeting + "\n");
+        System.out.println(exit);
+
     }
 }
