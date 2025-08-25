4a9be22c54ba29a08b39d9a22f8b6b8cc27d4ce3
Anthony Chow <wjchowanthony@gmail.com>
2025-08-25 22:51:23 +0800

    Complete level 2: Tasks

--- src/main/java/Conversal.java
+++ src/main/java/Conversal.java
@@ -7,13 +7,14 @@ public class Conversal {
         String name = "Conversal";
         String greeting = "What can I do for you?";
         String exit = "Bye! Hope to see you again!";
-        String instruction = "Enter bye to close chatbot.";
-
+        String instruction = "To close chatbot, enter: bye. To display tasks list, enter: list.";
 
         System.out.println("Hello! I'm " + name + ".");
         System.out.println(greeting + "\n");
         System.out.println("Instructions: " + instruction + "\n");
 
+        String[] tasks = new String[100];
+        int tasksCounter = 0;
         Scanner scanner = new Scanner(System.in);
         String input;
 
@@ -22,10 +23,26 @@ public class Conversal {
             input = scanner.nextLine();
 
             if (input.equals("bye")) {
-                break;  // Exit when user types Bye
+                // Exit when user types: bye
+                break;
+
+            } else if (input.equals("list")) {
+                // Print list when user types: list
+                System.out.println("\nHere is your list of tasks:");
+                for (int i = 0; i < tasksCounter; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println();
+
+            } else {
+                tasks[tasksCounter] = input;
+                tasksCounter++;
+                // Echo tasks added
+                System.out.println("Task added: " + input + "\n");
             }
 
-            System.out.println("Conversal: " + input + "\n");  // Repeat user input
+            // Repeat user input
+            //System.out.println("Conversal: " + input + "\n");
         }
 
         System.out.println("\n" + exit);
