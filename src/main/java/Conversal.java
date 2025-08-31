fcdf806b3797948c27c92858295cb60ca07bd9bc
Anthony Chow <wjchowanthony@gmail.com>
2025-08-31 23:45:37 +0800

    refractor: update Conversal for better readability

--- src/main/java/Conversal.java
+++ src/main/java/Conversal.java
@@ -1,31 +1,44 @@
-import java.time.LocalDate;
-
+// Main file
 public class Conversal {
 
-    public static void main(String[] args) {
-        Ui ui = new Ui();   // Create new user interface object
-        Storage storage = new Storage("./data/tasks.txt");  // Create Storage object
-        TaskList tasks = new TaskList(storage.load());  // Create new TaskList object with arraylist in storage
+    // Fields
+    private final Storage storage;
+    private final TaskList tasks;
+    private final Ui ui;
+
+    // Constructor
+    public Conversal(String filePath) {
+        ui = new Ui();    // Create new user interface object
+        storage = new Storage(filePath);   // Create Storage object
+        tasks = new TaskList(storage.load());
+    }
+
+    // Run the chatbot
+    public void run() {
         ui.welcomeMessage();    // Print welcome message
 
         while (true) {
-
             String input = ui.readInput();  // Read user input
 
             try {
-                Command cmd = Parser.parse(input);      // Parse input
-                cmd.execute(tasks, storage, ui);    // Execute command
+                Command cmd = Parser.parse(input);   // Parse input
+                cmd.execute(tasks, storage, ui);     // Execute command
 
                 if (cmd.isExit()) {
-                    break;      // Exit from while loop
+                    break;    // Exit from while loop
                 }
 
             } catch (ConversalException e) {
-                ui.printError(e.toString());    // Print error message
+                ui.printError(e.toString());   // Print error message
             }
         }
 
-        ui.exitMessage();   // print exit message
-        ui.close();     // Close scanner
+        ui.exitMessage();   // Print exit message
+        ui.close();         // Close scanner
+    }
+
+    // Main entry point
+    public static void main(String[] args) {
+        new Conversal("./data/tasks.txt").run();
     }
 }
