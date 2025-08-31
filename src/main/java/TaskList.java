36d7c8a6ae898abe845e547afe3f869b1f18dafd
Anthony Chow <wjchowanthony@gmail.com>
2025-08-31 22:14:03 +0800

    refractor: create TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+// More OOP - TaskList: contains the task list
+public class TaskList {
+
+    // Fields
+    protected final ArrayList<Task> tasks;
+
+    // Constructor
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    // Method 1: add task
+
+
+    // Method 2: delete task
+
+
+    // Method 3: get task
+
+
+    // Method 4: mark task as complete
+
+
+    // Method 5: mark task as incomplete
+
+
+    // Method 6: return task itself
+
+
+}
