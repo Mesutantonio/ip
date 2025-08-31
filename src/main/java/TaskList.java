ba6553f8016ab69338f447a506684a253dea1cb2
Anthony Chow <wjchowanthony@gmail.com>
2025-08-31 22:24:52 +0800

    refractor: basic TaskList class method skeleton

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class TaskList {
 
     // Fields
-    protected final ArrayList<Task> tasks;
+    private final ArrayList<Task> tasks;
 
     // Constructor
     public TaskList(ArrayList<Task> tasks) {
@@ -12,21 +12,33 @@ public class TaskList {
     }
 
     // Method 1: add task
-
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
 
     // Method 2: delete task
-
+    public void deleteTask(Task task) {
+        this.tasks.remove(task);
+    }
 
     // Method 3: get task
-
+    public Task getTask(int index) {
+        return this.tasks.get(index);
+    }
 
     // Method 4: mark task as complete
-
+    public void markTaskComplete(int index) {
+        this.tasks.get(index).markAsComplete();
+    }
 
     // Method 5: mark task as incomplete
+    public void markTaskIncomplete(int index) {
+        this.tasks.get(index).markAsIncomplete();
+    }
 
-
-    // Method 6: return task itself
-
+    // Method 6: return task list itself
+    public ArrayList<Task> getList() {
+        return this.tasks;
+    }
 
 }
