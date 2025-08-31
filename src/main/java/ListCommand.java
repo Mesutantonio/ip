f9a3fa9bcb53c80a3d2cc79c923ba608b4c05bff
Anthony Chow <wjchowanthony@gmail.com>
2025-08-31 23:12:25 +0800

    refractor: create ListCommand

---
+++ src/main/java/ListCommand.java
@@ -0,0 +1,13 @@
+// Command to list all tasks in task list
+public class ListCommand implements Command {
+
+    @Override
+    public void execute(TaskList tasks, Storage storage, Ui ui) throws ConversalException {
+        ui.showList(tasks.getList());
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
