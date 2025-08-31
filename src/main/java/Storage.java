a2a6f3c38056db371621a0a288eb5d41dbcb3556
Anthony Chow <wjchowanthony@gmail.com>
2025-08-31 16:59:46 +0800

    feat: Updated load method

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -2,8 +2,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-// Loads data from hard disc (and stores it) and saves data to hard disc
+// Loads data from hard disc and saves data to hard disc
 public class Storage {
     private String filePath;
 
@@ -11,25 +12,70 @@ public class Storage {
         this.filePath = filePath;
     }
 
-    // Loads data from hard disc and stores it
+    // Method 1: Loads data from hard disc and stores it in an arraylist to be returned
     public ArrayList<Task> load() {
         ArrayList<Task> loadedTasks = new ArrayList<>();
+        File file = new File(filePath);
+
+        // If file does not exist, return empty list
+        if (!file.exists()) {
+            return loadedTasks;
+        }
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            // For each data entry
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" \\| ");
+
+                boolean isDone = parts[1].equals("1");
+                Task newTask;
+
+                // Create correct task
+                switch (parts[0]) {
+                    case "D": // Deadline task
+                        newTask = new Deadline(parts[2], parts[3]);
+                        break;
+                    case "E": // Event task
+                        String[] subParts = parts[3].split("-", 2);
+                        newTask = new Event(parts[2], subParts[0], subParts[1]); // parts[3] is the 'at' string
+                        break;
+                    default: // Normal task
+                        newTask = new Todo(parts[2]);
+                        break;
+                }
+
+                // Add completion status
+                if (isDone) {
+                    newTask.markAsComplete();
+                }
+
+                // Add task to the list
+                loadedTasks.add(newTask);
+            }
+            scanner.close();
+
+        } catch (IOException e) {
+            System.out.println("Unable to load task: " + e.getMessage());
+        }
 
         return loadedTasks;
     }
 
-    // Saves data to hard disc
+    // Method 2: Saves data to hard disc
     public void save(ArrayList<Task> tasks) {
         try {
             File file = new File(this.filePath);
 
-            // Create the new directory if file does not exist
+            // Create the new directory if directory does not exist
             File parentDir = file.getParentFile();
             if (parentDir != null && !parentDir.exists()) {
                 parentDir.mkdirs();
             }
 
-            // Write to the file
+            // Write to created file
             FileWriter fw = new FileWriter(file);
             for (Task task : tasks) {
 
