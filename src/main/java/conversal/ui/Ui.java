9111a63437bc4f1b981d295b9c33f406ecbba686
Anthony Chow <wjchowanthony@gmail.com>
2025-09-01 16:13:36 +0800

    style: apply consistent code standard and fix typos

--- src/main/java/conversal/ui/Ui.java
+++ src/main/java/conversal/ui/Ui.java
@@ -10,17 +10,23 @@ public class Ui {
 
     // Fields
     protected Scanner scanner;
-    protected String name = "conversal.Conversal";
-    protected String greeting = "What can I do for you?";
-    protected String exit = "Bye! Hope to see you again!";
-    protected String instructionBye = "To close chatbot, enter: bye.";
-    protected String instructionList = "To display tasks list, enter: list";
-    protected String instructionMark = "To mark task as Complete, enter: mark (task no.)";
-    protected String instructionUnmark = "To mark task as Incomplete, enter: unmark (task no.)";
-    protected String instructionDelete = "To delete a task: delete (task no.)";
-    protected String instructionTodo = "To add conversal.task.Todo task, enter: todo (task)";
-    protected String instructionDeadline = "To add conversal.task.Deadline task, enter: deadline (task) /by (date in YYYY-MM-DD format)";
-    protected String instructionEvent = "To add conversal.task.Event task, enter: event (task) /from (start) /to (end)";
+    protected final static String name = "conversal.Conversal";
+    protected final static String greeting = "What can I do for you?";
+    protected final static String exit = "Bye! Hope to see you again!";
+    protected final static String instructionBye = "To close chatbot, enter: bye.";
+    protected final static String instructionList = "To display tasks list, enter: list";
+    protected final static String instructionMark =
+            "To mark task as Complete, enter: mark (task no.)";
+    protected final static String instructionUnmark =
+            "To mark task as Incomplete, enter: unmark (task no.)";
+    protected final static String instructionDelete =
+            "To delete a task: delete (task no.)";
+    protected final static String instructionTodo =
+            "To add conversal.task.Todo task, enter: todo (task)";
+    protected final static String instructionDeadline =
+            "To add conversal.task.Deadline task, enter: deadline (task) /by (date in YYYY-MM-DD format)";
+    protected final static String instructionEvent =
+            "To add conversal.task.Event task, enter: event (task) /from (start) /to (end)";
 
     // Constructor
     public Ui() {
@@ -35,23 +41,23 @@ public class Ui {
 
     // Method 2: print welcome message and instructions
     public void welcomeMessage() {
-        System.out.println("Hello! I'm " + this.name + ".");
-        System.out.println(this.greeting + "\n");
+        System.out.println("Hello! I'm " + name + ".");
+        System.out.println(greeting + "\n");
         System.out.println("Instructions: ");
-        System.out.println("> " + this.instructionBye);
-        System.out.println("> " + this.instructionList);
-        System.out.println("> " + this.instructionMark);
-        System.out.println("> " + this.instructionUnmark);
-        System.out.println("> " + this.instructionDelete);
-        System.out.println("> " + this.instructionTodo);
-        System.out.println("> " + this.instructionDeadline);
-        System.out.println("> " + this.instructionEvent);
+        System.out.println("> " + instructionBye);
+        System.out.println("> " + instructionList);
+        System.out.println("> " + instructionMark);
+        System.out.println("> " + instructionUnmark);
+        System.out.println("> " + instructionDelete);
+        System.out.println("> " + instructionTodo);
+        System.out.println("> " + instructionDeadline);
+        System.out.println("> " + instructionEvent);
         System.out.println();
     }
 
     // Method 3: print exit message
     public void exitMessage() {
-        System.out.println("\n" + this.exit);
+        System.out.println("\n" + exit);
     }
 
     // Method 4: print message when task is added
