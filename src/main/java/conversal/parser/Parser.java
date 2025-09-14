db1774ec18fe3d6934977ada2b7ed4f8e8a25c62
Anthony Chow <wjchowanthony@gmail.com>
2025-09-14 12:05:27 +0800

    feat(DoWithinPeriodTask): update parse method in parse

--- src/main/java/conversal/parser/Parser.java
+++ src/main/java/conversal/parser/Parser.java
@@ -48,6 +48,8 @@ public class Parser {
             return new DeadlineCommand(input);
         } else if (input.startsWith("event ")) {
             return new EventCommand(input);
+        } else if (input.startsWith("within ")) {
+                return new DoWithinCommand(input);
         } else {
             throw new ConversalException("I can't seem to locate the issue, please try again!");
         }
